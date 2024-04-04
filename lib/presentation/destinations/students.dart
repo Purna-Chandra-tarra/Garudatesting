54f3b786e130a4ea2e55171e4f40f4a05be97e41
Dipansh Parmar <dipanshparmar870@gmail.com>
2024-04-04 16:37:23 +0530

    fixed scrolling issues

--- lib/presentation/destinations/students.dart
+++ lib/presentation/destinations/students.dart
@@ -11,125 +11,117 @@ class Students extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     UserDatabase userDatabase = ref.watch(userDatabaseProvider);
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(28.0),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width - 320,
-          height: MediaQuery.of(context).size.height,
-          child: ListView(
-            children: [
-              Text(
-                'Students',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: StreamBuilder(
-                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(child: CupertinoActivityIndicator());
-                    } else {
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            child: ListTile(
-                              trailing: FilledButton(
-                                onPressed: () async {
-                                  ref
-                                      .read(userDatabaseProvider)
-                                      .changeStudentStatus(snapshot
-                                          .data?.docs[index]['phone_no']);
-                                },
-                                child: const Text("Reset"),
-                              ),
-                              title: Text(
-                                "Student Name: ${snapshot.data?.docs[index]['name']}",
-                              ),
-                              subtitle: GestureDetector(
-                                onTap: () async {
-                                  String phoneNo = await snapshot
-                                      .data?.docs[index]['phone_no'];
-                                  await ref
-                                      .watch(userDatabaseProvider)
-                                      .changeStudentStatus(phoneNo);
-                                },
-                                child: Row(
-                                  children: [
-                                    GestureDetector(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          color: snapshot.data?.docs[index]
-                                                      ['device_id'] !=
-                                                  "error"
-                                              ? Colors.green
-                                              : Colors.red,
-                                          borderRadius:
-                                              BorderRadius.circular(3),
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(3.0),
-                                          child: Text(
-                                            snapshot.data?.docs[index]
-                                                        ['device_id'] !=
-                                                    "error"
-                                                ? "ACTIVE"
-                                                : 'INACTIVE',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .bodySmall
-                                                ?.copyWith(
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .primary,
-                                        borderRadius: BorderRadius.circular(3),
-                                      ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(3.0),
-                                        child: SelectableText(
-                                          snapshot.data?.docs[index]
-                                              ['phone_no'],
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodySmall
-                                              ?.copyWith(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                        ),
-                                      ),
-                                    ),
-                                    const Spacer(),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                        itemCount: snapshot.data?.docs.length,
-                      );
-                    }
-                  },
-                  stream: userDatabase.getUserList(),
+    return Padding(
+      padding: const EdgeInsets.all(28.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Students',
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
                 ),
-              ),
-              const SizedBox(height: 16),
-            ],
           ),
-        ),
+          StreamBuilder(
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CupertinoActivityIndicator());
+              } else {
+                return Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.only(top: 20),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          trailing: FilledButton(
+                            onPressed: () async {
+                              ref
+                                  .read(userDatabaseProvider)
+                                  .changeStudentStatus(
+                                      snapshot.data?.docs[index]['phone_no']);
+                            },
+                            child: const Text("Reset"),
+                          ),
+                          title: Text(
+                            "Student Name: ${snapshot.data?.docs[index]['name']}",
+                          ),
+                          subtitle: GestureDetector(
+                            onTap: () async {
+                              String phoneNo =
+                                  await snapshot.data?.docs[index]['phone_no'];
+                              await ref
+                                  .watch(userDatabaseProvider)
+                                  .changeStudentStatus(phoneNo);
+                            },
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: snapshot.data?.docs[index]
+                                                  ['device_id'] !=
+                                              "error"
+                                          ? Colors.green
+                                          : Colors.red,
+                                      borderRadius: BorderRadius.circular(3),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(3.0),
+                                      child: Text(
+                                        snapshot.data?.docs[index]
+                                                    ['device_id'] !=
+                                                "error"
+                                            ? "ACTIVE"
+                                            : 'INACTIVE',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall
+                                            ?.copyWith(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 5),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    borderRadius: BorderRadius.circular(3),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(3.0),
+                                    child: SelectableText(
+                                      snapshot.data?.docs[index]['phone_no'],
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall
+                                          ?.copyWith(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                ),
+                                const Spacer(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: snapshot.data?.docs.length,
+                  ),
+                );
+              }
+            },
+            stream: userDatabase.getUserList(),
+          ),
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
