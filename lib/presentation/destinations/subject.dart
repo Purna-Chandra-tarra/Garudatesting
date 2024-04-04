0ca69d684b7e0368f7824c1b1acff5e52c7fab76
Dipansh Parmar <dipanshparmar870@gmail.com>
2024-04-04 17:29:25 +0530

    fixed scrollview in subject page

--- lib/presentation/destinations/subject.dart
+++ lib/presentation/destinations/subject.dart
@@ -18,102 +18,98 @@ class Subject extends ConsumerWidget {
     ExamDatabase examDatabase = ref.watch(examDatabaseProvider);
     return Padding(
       padding: const EdgeInsets.all(28.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width - 320,
-        child: ListView(
-          children: [
-            Text(
-              'Subject',
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: StreamBuilder(
-                stream: examDatabase.getSubject(examId),
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: snapshot.data.docs.length,
-                      itemBuilder: ((context, index) {
-                        return Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Row(
-                              children: [
-                                Text(snapshot.data.docs[index]['name']),
-                                const Spacer(),
-                                FutureBuilder(
-                                  builder: (context, snapshot) {
-                                    if (snapshot.hasData) {
-                                      return Text(snapshot.data.toString());
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Subject',
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          StreamBuilder(
+            stream: examDatabase.getSubject(examId),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: ((context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(snapshot.data.docs[index]['name']),
+                              ),
+                              const Spacer(),
+                              FutureBuilder(
+                                builder: (context, snapshot) {
+                                  if (snapshot.hasData) {
+                                    return Text(snapshot.data.toString());
+                                  } else {
+                                    return const CupertinoActivityIndicator();
+                                  }
+                                },
+                                future: ref
+                                    .watch(examDatabaseProvider)
+                                    .getSubjectLength(examId,
+                                        snapshot.data.docs[index]['name']),
+                              ),
+                              FutureBuilder(
+                                builder: (context, AsyncSnapshot snapshot) {
+                                  if (snapshot.hasData) {
+                                    if (snapshot.data) {
+                                      return IconButton(
+                                        onPressed: () async {
+                                          showLoaderDialog(context);
+                                          await examDatabase.deleteExamSubject(
+                                            examId,
+                                            snapshot.data.docs[index].id,
+                                          );
+                                          Navigator.pop(context);
+                                        },
+                                        icon: Icon(
+                                          Icons.delete,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .error,
+                                        ),
+                                      );
                                     } else {
-                                      return const CupertinoActivityIndicator();
+                                      return const SizedBox();
                                     }
-                                  },
-                                  future: ref
-                                      .watch(examDatabaseProvider)
-                                      .getSubjectLength(examId,
-                                          snapshot.data.docs[index]['name']),
-                                ),
-                                FutureBuilder(
-                                  builder: (context, AsyncSnapshot snapshot) {
-                                    if (snapshot.hasData) {
-                                      if (snapshot.data) {
-                                        return IconButton(
-                                          onPressed: () async {
-                                            showLoaderDialog(context);
-                                            await examDatabase
-                                                .deleteExamSubject(
-                                              examId,
-                                              snapshot.data.docs[index].id,
-                                            );
-                                            Navigator.pop(context);
-                                          },
-                                          icon: Icon(
-                                            Icons.delete,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .error,
-                                          ),
-                                        );
-                                      } else {
-                                        return const SizedBox();
-                                      }
-                                    } else {
-                                      return const CupertinoActivityIndicator();
-                                    }
-                                  },
-                                  future: ref
-                                      .watch(userDatabaseProvider)
-                                      .isSuperUser(
-                                        ref
-                                            .watch(authServiceProvider)
-                                            .user!
-                                            .email
-                                            .toString(),
-                                      ),
-                                ),
-                              ],
-                            ),
+                                  } else {
+                                    return const CupertinoActivityIndicator();
+                                  }
+                                },
+                                future:
+                                    ref.watch(userDatabaseProvider).isSuperUser(
+                                          ref
+                                              .watch(authServiceProvider)
+                                              .user!
+                                              .email
+                                              .toString(),
+                                        ),
+                              ),
+                            ],
                           ),
-                        );
-                      }),
-                    );
-                  } else {
-                    return const CupertinoActivityIndicator();
-                  }
-                },
-              ),
-            )
-          ],
-        ),
+                        ),
+                      );
+                    }),
+                  ),
+                );
+              } else {
+                return const CupertinoActivityIndicator();
+              }
+            },
+          )
+        ],
       ),
     );
   }
